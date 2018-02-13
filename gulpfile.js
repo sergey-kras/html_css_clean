4c7e08fd6ddc665565b69e8d91c894adc4c8e1af
Sergey Krasnolutskiy <vip.41243@gmail.com>
2018-02-13 22:55:06 +0700

    Add js concat, uglify, dir with libs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concatCss = require('gulp-concat-css');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
 
 gulp.task('sass', function () { // sass компилятор
     return gulp.src('./app/scss/*.scss')
@@ -25,6 +28,12 @@ gulp.task('ReMAP', function () {
     gulp.src('./app/css/all/*.map') // Перемещаем .map
         .pipe(gulp.dest('./app/css'))
 });
-gulp.task('watch', function () {
-    gulp.watch('./app/scss/*.scss', ['sass', 'ReCSS']);
-})
+gulp.task('libs', function() {
+    return gulp.src([
+        'app/js/libs/*.js',
+        'app/js/common.js'
+    ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+});
