17204e1367552917479c29cc5ef1ac9df8ad1a0b
Sergey Krasnolutskiy <vip.41243@gmail.com>
2018-02-19 17:22:46 +0700

    Add smart-grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,43 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var concatCss = require('gulp-concat-css');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
+var smartgrid = require('smart-grid');
 
-gulp.task('sass', function () {
+gulp.task('smartgrid', function () {
+    /* It's principal settings in smart grid project */
+    var settings = {
+        outputStyle: 'scss', /* less || scss || sass || styl */
+        columns: 12, /* number of grid columns */
+        offset: '30px', /* gutter width px || % */
+        mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+        container: {
+            maxWidth: '1200px', /* max-width оn very large screen */
+            fields: '30px' /* side fields */
+        },
+        breakPoints: {
+            lg: {
+                width: '1100px', /* -> @media (max-width: 1100px) */
+            },
+            md: {
+                width: '960px'
+            },
+            sm: {
+                width: '780px',
+                fields: '15px' /* set fields only if you want to change container.fields */
+            },
+            xs: {
+                width: '560px'
+            }
+        }
+    };
+    smartgrid('./app/scss/', settings);
+});
+gulp.task('sass',['smartgrid'], function () {
     gulp.src('./app/scss/main.scss')
         .pipe(autoprefixer()) // автопрефикс
         .pipe(sass(
