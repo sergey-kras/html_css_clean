46d1ec7003bb045fa606a14df86ae209cff581e5
Sergey Krasnolutskiy <vip.41243@gmail.com>
2018-02-19 17:08:45 +0700

    Add watcher and browser-synk

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,20 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     gulp.src('./app/scss/main.scss')
         .pipe(autoprefixer()) // автопрефикс
         .pipe(sass(
             {
-                // includePaths: require('node-normalize-scss').with('other/path', 'another/path')
-                // - or -
                 includePaths: require('node-normalize-scss').includePaths,
                 outputStyle: 'compressed' // тип выхода
         }))
-        .pipe(gulp.dest('./dist/css/'));
+        .pipe(gulp.dest('./dist/css/'))
 });
 gulp.task('html', function () {
-    gulp.src('./app/*.html').pipe(gulp.dest('./dist/'))
+    gulp.src('./app/*.html').pipe(gulp.dest('./dist/'));
 });
 gulp.task('libs', function() {
     return gulp.src([
@@ -30,10 +29,34 @@ gulp.task('libs', function() {
     ])
         .pipe(concat('scripts.min.js')) // Собираем их в кучу в новом файле libs.min.js
         .pipe(uglify()) // Сжимаем JS файл
-        .pipe(gulp.dest('./dist/js')); // Выгружаем в папку app/js
+        .pipe(gulp.dest('./dist/js')) // Выгружаем в папку app/js
 });
 gulp.task('compress', function() {
     gulp.src('./app/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/img/'))
 });
+gulp.task('watcher', function() {
+    gulp.watch("./app/scss/*.scss", ['sass']);
+    gulp.watch("./app/js/*.js", ['libs']);
+    gulp.watch("./app/img/*", ['compress']);
+    gulp.watch("./app/*.html", ['html']);
+});
+gulp.task('browser', function () {
+    var files = [
+        './**/*'
+    ];
+    browserSync.init( files,
+        {
+            injectChanges: true,
+            server: "./dist",
+            open: false,
+            notify: false,
+            port: 8080,
+            watchOptions : {
+                ignored : 'node_modules/*',
+                ignoreInitial : true
+            }
+    });
+})
+gulp.task('server', ['browser','watcher'])
